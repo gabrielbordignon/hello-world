a89db2b0ce7442b6e854765cf7d9b7a972d6aea9
gabrielbordignon <gabriel.bordignon28@escola.pr.gov.br>
2025-06-26 18:18:27 -0300

    Create classificaNumero.js

---
+++ classificaNumero.js
@@ -0,0 +1,9 @@
+let numero = 0
+
+if (numero > 0){
+    console.log("O numero é positivo!");
+}else if (numero < 0){
+    console.log("O numero é negativo");
+}else if (numero == 0){
+    console.log("O numero é 0")
+}

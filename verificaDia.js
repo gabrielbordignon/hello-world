2f4b63ee9e7d3068a3290409afadc7f8db4ecad9
gabrielbordignon <gabriel.bordignon28@escola.pr.gov.br>
2025-06-26 18:16:59 -0300

    Create  verificaDia.js

---
+++ verificaDia.js
@@ -0,0 +1,7 @@
+let dia = "sabado";
+
+if (dia === "sabado" ||dia === "domingo") {
+    console.log("Hoje é fim de semana! bora Beber!");
+}else {
+    console.log("hoje é dia de semana, bora trabalhar!");
+}

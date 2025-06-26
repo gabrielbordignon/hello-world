d3033caec1f3206a1eddba5c867a034b84e63bb3
gabrielbordignon <gabriel.bordignon28@escola.pr.gov.br>
2025-06-26 18:17:42 -0300

    Create verificaSenha.js

---
+++ verificaSenha.js
@@ -0,0 +1,8 @@
+let senhaVerdadeira = "12345"
+let senhaDigitada = "43254"
+
+if (senhaDigitada === senhaVerdadeira) {
+    console.log("Login bem-sucedido");
+}else {
+    console.log("senha incorreta, tente novamente")
+}

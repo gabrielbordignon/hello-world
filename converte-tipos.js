916d01dde0436e1faeb7f4f746d1275ddde6e55b
gabrielbordignon <gabriel.bordignon28@escola.pr.gov.br>
2025-07-16 17:54:40 -0300

    Create converte-tipos.js

---
+++ converte-tipos.js
@@ -0,0 +1,12 @@
+// Entrada de string
+let numeroString = "123";
+console.log('Número inicial: ', typeof numeroString); // Saída: string
+
+// Convertendo para número inteiro
+let numeroInteiro = parseInt(numeroString);
+console.log('1a. conversao para inteiro: ', typeof numeroInteiro); // Saída: number
+
+// Convertendo para número decimal (float)
+let numeroDecimalString = "123.45";
+let numeroDecimal = parseFloat(numeroDecimalString);
+console.log('2a. conversao para decimal: ', typeof numeroDecimal); // Saída: number

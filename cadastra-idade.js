f8ec66f4db945ab8e1b2fea359710e469f062277
gabrielbordignon <gabriel.bordignon28@escola.pr.gov.br>
2025-07-16 17:51:24 -0300

    Create cadastra-idade.js

---
+++ cadastra-idade.js
@@ -0,0 +1,8 @@
+// Solicita o nome do usuário
+let nome = prompt("Digite seu nome:");
+
+// Solicita a idade do usuário e converte para número
+let idade = parseInt(prompt("Digite sua idade:"));
+
+// Exibe as informações coletadas
+console.log(`Olá ${nome}, você tem ${idade} anos.`);

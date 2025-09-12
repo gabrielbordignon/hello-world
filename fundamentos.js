6e2e59ea80cee73f4c287918dfaa97719dd4d5fc
gabrielbordignon <gabriel.bordignon28@escola.pr.gov.br>
2025-09-12 15:25:47 -0300

    Create fundamentos.js

---
+++ fundamentos.js
@@ -0,0 +1,33 @@
+let idade = 30
+
+console.log(idade)
+
+function soma(num1, num2) {
+    return num1+num2
+}
+
+console.log(soma(1, 2))
+
+
+let num = 7 
+
+if (num /2 === 0) {
+    console.log("par")
+}
+else {
+    console.log("impar")
+}
+
+
+for (let i = 65; i <= 90; i++) {
+    console.log(String.fromCharCode(i));
+}
+
+
+function media(n1,n2,n3) {
+    return (n1+n2+n3) / 2
+}
+console.log(media(15,43,54))
+
+
+console.log("Hello world!");
